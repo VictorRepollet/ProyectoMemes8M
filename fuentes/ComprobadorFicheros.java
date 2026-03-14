c5b42043a868e65239dbc599c97b8ffe3c4c6c6f
VictorCasa <victorrepolletcumplido@iescastelar.com>
2026-03-14 21:16:31 +0100

    HU1 completada

---
+++ fuentes/ComprobadorFicheros.java
@@ -0,0 +1,67 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * Clase encargada de verificar la existencia de los recursos necesarios
+ * para el correcto funcionamiento de la aplicación.
+ *
+ * Comprueba que el directorio <code>datos</code> exista y que dentro de él
+ * se encuentren los ficheros requeridos por el programa:
+ * <ul>
+ *     <li>memes.txt</li>
+ *     <li>realidades.json</li>
+ *     <li>soluciones.xml</li>
+ * </ul>
+ *
+ * En caso de que alguno de estos recursos no exista, se lanzará una excepción
+ * indicando cuál es el elemento faltante.
+ *
+ * @author Victor, Alejandro, Javier
+ * @version 1.0
+ */
+public class ComprobadorFicheros {
+
+    /**
+     * HU1 - Verificación de archivos necesarios.
+     *
+     * Este método comprueba que exista el directorio <code>datos</code> y que
+     * dentro de él se encuentren los ficheros necesarios para la ejecución
+     * de la aplicación.
+     *
+     * Si el directorio o alguno de los ficheros no existe, se lanza una
+     * excepción {@link FileNotFoundException} indicando qué recurso falta.
+     *
+     * @throws Exception si el directorio <code>datos</code> o alguno de los
+     *                   archivos requeridos no se encuentra en la ruta esperada
+     */
+    public void comprobarDatos() throws Exception {
+
+        // Referencia al directorio datos
+        File directorioDatos = new File("../datos");
+
+        // Referencias a los archivos que deben existir en la carpeta datos
+        File memes = new File("../datos/memes.txt");
+        File realidades = new File("../datos/realidades.json");
+        File soluciones = new File("../datos/soluciones.xml");
+
+        // Comprobación del directorio datos
+        if (!directorioDatos.exists() || !directorioDatos.isDirectory()) {
+            throw new FileNotFoundException("No se encontro el directorio \"datos/\".");
+        }
+
+        // Comprobación del archivo memes.txt
+        if (!memes.exists()) {
+            throw new FileNotFoundException("No se encontro el fichero \"memes.txt\".");
+        }
+
+        // Comprobación del archivo realidades.json
+        if (!realidades.exists()) {
+            throw new FileNotFoundException("No se encontro el fichero \"realidades.json\".");
+        }
+
+        // Comprobación del archivo soluciones.xml
+        if (!soluciones.exists()) {
+            throw new FileNotFoundException("No se encontro el fichero \"soluciones.xml\".");
+        }
+    }
+}
