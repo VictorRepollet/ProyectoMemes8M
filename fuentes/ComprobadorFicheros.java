9c05d6afe28c3025e7873778f0de41daee6024b7
VictorCasa <victorrepolletcumplido@iescastelar.com>
2026-03-14 21:46:06 +0100

    hu2 terminada

--- fuentes/ComprobadorFicheros.java
+++ fuentes/ComprobadorFicheros.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Clase encargada de verificar la existencia de los recursos necesarios
@@ -62,6 +63,42 @@ public class ComprobadorFicheros {
         // Comprobación del archivo soluciones.xml
         if (!soluciones.exists()) {
             throw new FileNotFoundException("No se encontro el fichero \"soluciones.xml\".");
+        }
+    }
+
+    /**
+     * HU2
+     * Comprueba si existe el directorio resultados y el fichero resultados.txt.
+     * Si no existen, los crea automáticamente.
+     */
+    public void comprobarResultados(){
+
+        // Objeto File que representa la carpeta resultados
+        File carpeta = new File("../resultados");
+
+        // Si la carpeta no existe se crea
+        if(!carpeta.exists()){
+            System.out.println("\nCarpeta resultados no existe. Creándola...");
+            carpeta.mkdir();
+        }
+
+        // Referencia al archivo resultados.txt dentro de la carpeta resultados
+        File archivo = new File(carpeta + "/resultados.txt");
+
+        // Si el archivo existe se informa al usuario
+        if(!archivo.exists()){
+            try{
+                // Se crea el archivo si no existe
+                archivo.createNewFile();
+                System.out.println("resultados.txt creado correctamente");
+            }
+            catch(IOException e){
+                // Manejo de error si ocurre un problema al crear el archivo
+                System.out.println("Error al crear resultados.txt");
+            }
+        } else {
+            System.out.println("resultados.txt ya existe");
+
         }
     }
 }
